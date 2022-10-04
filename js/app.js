190d19084751789e4e05ecadcefd92d4c4530ee7
Alfredo Sánchez Sánchez <alfsan04@gmail.com>
2022-10-04 20:45:56 +0200

    Añadimos filas a una tabla y gestión de clicks

--- js/app.js
+++ js/app.js
@@ -1,11 +1,17 @@
 
-function cuentaClicks(event) {
-    let input = document.querySelector("#entrada").value
-    alert(input)
+function addRow(event) {
+    const texto = document.querySelector("#entrada").value
+    const tabla = document.querySelector("#la_tabla")
+    const fila = document.createElement("tr")
+    const celda = document.createElement("td")
+    celda.innerHTML = texto
+
+    fila.appendChild(celda)
+    tabla.appendChild(fila)
 }
 
 window.onload = function() {
     let elboton = document.querySelector("#crear")
-    elboton.addEventListener("click", cuentaClicks)
+    elboton.addEventListener("click", addRow)
     
 }
